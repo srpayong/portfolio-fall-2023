63965f36b832fb072b77de723608d469b0f91486
Sharmaine Payong <sharmainerpayong@gmail.com>
2023-09-04 17:13:15 +0200

    Update tsconfig file

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,7 +15,7 @@ function Home() {
         <div className="container">
           <div className="text-wrapper">
             <h1 data-test-id="pageTitle" className="title">
-              Halo, saya Lutfhi Ramadhan
+              Hello, Sharmaine!
             </h1>
             <p className="description">
               Saya seorang pekerja lepas pengembang web, fotograper, desainer
@@ -23,7 +23,7 @@ function Home() {
             </p>
 
             <Link href="/contact" className="cta">
-              Kontak Saya
+              Contact me
             </Link>
           </div>
 
